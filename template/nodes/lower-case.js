9eccd5e80309039961f57292dcff3a4a686b70ed
eloimenaud <menaudeloi@gmail.com>
2022-07-14 10:29:02 +0200

    fix: architecture

--- template/nodes/lower-case.js
+++ template/nodes/lower-case.js
@@ -1,12 +1,11 @@
 var smallest = require('smallest')
- 
 
 module.exports = function(RED){
     function LowerCaseNode(config){
         RED.nodes.createNode(this,config);
         var node = this;
         node.on('input', function(msg) {
-            msg.payload = msg.payload.toLowerCase() + "55" + smallest(1, 2, 4, 3).toString();
+            msg.payload = msg.payload.toLowerCase() + smallest(1, 2, 4, 3).toString();
             node.send(msg);
         });
     }
