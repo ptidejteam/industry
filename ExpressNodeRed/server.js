6fc128d140bf933e77837032e3f15668daf36ac6
timvtinsa <timothe.verstraete@insa-lyon.fr>
2022-07-21 11:40:06 +0200

    Update API with fitbit treatment moved to node red

--- ExpressNodeRed/server.js
+++ ExpressNodeRed/server.js
@@ -56,13 +56,10 @@ function prmsRequest(url){
     })
 }
 
-
-
-
-app.get('/fit',async(req,res)=>{
+app.get('/fitbit',async(req,res)=>{
   var resp = ""
   try{
-      resp = await prmsRequest('http://localhost:8000/api/fit');
+      resp = await prmsRequest('http://localhost:8000/api/fitbit');
   }
   catch (e){
       console.log(e);
@@ -75,84 +72,6 @@ app.get('/',async(req,res)=>{
   console.log('server-test')
   res.send("Server OK");
 });
-
-let lastFitbitData = "";
-
-
-app.post('/fitbit',async(req,res)=>{
-  let data = {};
-  if (db.has("fitbit-" + lastFitbitData)){
-    data = db.get("fitbit-" + lastFitbitData);
-  } else {
-    data = {
-      status: "",
-      location: {
-        latitude: "",
-        longitude: ""
-      },
-      presence: "",
-      acceleration: {
-        x: "",
-        y: "",
-        z: ""
-      },
-      lastSyncTime: "",
-      heartRate: "",
-      batteryLevel: "",
-      charge: ""
-    };
-  }
- 
-  let input = req.body;
-
-  if (input.hasOwnProperty("status")) {
-    data.status = input.status;
-  } else if (input.hasOwnProperty("location")) {
-    data.location.latitude = input.location.latitude;
-    data.location.longitude = input.location.longitude;
-  } else if (input.hasOwnProperty("presence")) {
-    data.presence = input.presence;
-  } else if (input.hasOwnProperty("acceleration")) {
-    data.acceleration.x = input.acceleration.x;
-    data.acceleration.y = input.acceleration.y;
-    data.acceleration.z = input.acceleration.z;
-  } else if (input.hasOwnProperty("lastSyncTime")) {
-    data.lastSyncTime = input.lastSyncTime;
-  } else if (input.hasOwnProperty("heartRate")) {
-    data.heartRate = input.heartRate;
-  } else if (input.hasOwnProperty("batteryLevel")) {
-    data.batteryLevel = input.batteryLevel;
-  } else if (input.hasOwnProperty("charge")) {
-    data.charge = input.charge;
-  }
-
-  lastFitbitData = new Date().getTime();
-  data.timestamp = lastFitbitData;
-  console.log(data);
-  db.set("fitbit-" + data.timestamp,data);
-
-  request.post('http://192.168.0.200:8000/api/fit', {json: data}, function(err, httpResponse, body) {
-    if (!err && httpResponse.statusCode == 200) {
-      console.log(body);
-    }
-  });
-  res.status(200).send({"received": "true"});
-});
-
-app.get("/demo",async(req,res)=>{
-  let data = {};
-  console.log(lastFitbitData);
-  if (db.has("fitbit-" + lastFitbitData)){
-    data = db.get("fitbit-" + lastFitbitData);
-  } else {
-    data = {
-      error: "No data found"
-    }
-  }
-  res.render(__dirname + "/demo/demo", { info: JSON.stringify(data, null, 2) });
-});
-
-
 
 server.listen(8000);
 
